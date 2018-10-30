4f0e0b549bb530e0bbb6502454237c39041639d5
Kurt <kurt@FuBar.fios-router.home>
2018-10-30 17:12:46 -0400

    added another spanish word for elevator, common in Peru

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Eris = require('eris');
  
 const bot = new Eris(process.env.DISCORD_BOT_TOKEN);   // Replace DISCORD_BOT_TOKEN in .env with your bot accounts token
  
-const elevatorWords = ['Elevator', 'elevator', 'acensor', 'Acensor', 'lift', 'Lift', 'illav8r', '엘리베이터', '电梯', 'aufzug', 'Aufzug']
+const elevatorWords = ['Elevator', 'elevator', 'elevador', 'Elevador', 'acensor', 'Acensor', 'lift', 'Lift', 'illav8r', '엘리베이터', '电梯', 'aufzug', 'Aufzug']
 
 bot.on('ready', () => {                                // When the bot is ready
     console.log('Ready!');                             // Log "Ready!"
